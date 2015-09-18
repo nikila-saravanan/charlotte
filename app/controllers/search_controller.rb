fb77a040f63186dc2acbdff5f78e399fc9af4d71
GabiOC <oconnor.gabriele@gmail.com>
2015-09-18 17:37:30 -0400

    Remove unnecessary numbers gsub

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,8 +4,9 @@ class SearchController < ApplicationController
   end
 
   def show
-    # binding.pry
-    sanitized_search = I18n.transliterate(params[:q]).gsub(/(\W|\d)/, " ")
+    # replace non-ASCII characters with ASCII and non-word characters with spaces for API call
+    sanitized_search = I18n.transliterate(params[:q]).gsub(/(\W)/, " ")
+
     @artist = Search.for(sanitized_search).first
     @related_artists = @artist.related_artists.sort_by {|artist| artist.followers["total"]}.reverse
   end
