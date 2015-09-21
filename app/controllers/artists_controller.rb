e9a224ab4ddbe68e8eb97b1943711cc3f795b6e0
nikila-saravanan <nikila.saravanan@gmail.com>
2015-09-21 13:54:14 -0400

    add favorite functionality

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,6 +1,11 @@
 class ArtistsController < ApplicationController
   def show
-    @artist = RSpotify::Artist.search(params[:q])[0]
-    # binding.pry
   end
+
+  def new
+    @artist = Artist.create(name: params["artist_name"],url: params["artist_url"],followers: params["artist_followers"])
+    current_user.artists << @artist
+    redirect_to user_path(current_user.id)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ActiveRecord::Base
   has_many :artists, through: :user_artists
   has_many :artist_genres
   has_many :genres, through: :artist_genres
+
+  
 end
