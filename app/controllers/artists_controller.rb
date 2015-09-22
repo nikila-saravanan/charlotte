40460c2c51e43b4044d4dc5dd7ed847ba1ae98b2
nikila-saravanan <nikila.saravanan@gmail.com>
2015-09-22 10:00:24 -0400

    fix user artist logic in artists controller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -5,10 +5,13 @@ class ArtistsController < ApplicationController
   def new
     artist = Artist.find_by(name: params["artist_name"])
     # @artist = Artist.find_or_create_by(name: params["artist_name"],url: params["artist_url"],followers: params["artist_followers"])
-    new_artist = Artist.create(name: params["artist_name"],url: params["artist_url"],followers: params["artist_followers"]) unless artist
     if current_user.artists.find_by(name: params["artist_name"])
       flash[:alert] = "#{artist.name} is already in your favorites."
+    elsif !current_user.artists.find_by(name: artist.name)
+      current_user.artists << artist
+      flash[:success] =  "#{artist.name} has been added to your favorites."
     else
+      new_artist = Artist.create(name: params["artist_name"],url: params["artist_url"],followers: params["artist_followers"]) unless artist
       current_user.artists << new_artist
       flash[:success] =  "#{new_artist.name} has been added to your favorites."
     end
