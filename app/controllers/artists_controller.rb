2c5d4670db8f8be341537fb2c0e740ff239b080c
nikila-saravanan <nikila.saravanan@gmail.com>
2015-09-22 14:19:05 -0400

    fix favorite functionality to bypass nil artist

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,16 +4,15 @@ class ArtistsController < ApplicationController
 
   def new
     artist = Artist.find_by(name: params["artist_name"])
-    # @artist = Artist.find_or_create_by(name: params["artist_name"],url: params["artist_url"],followers: params["artist_followers"])
-    if current_user.artists.find_by(name: params["artist_name"])
-      flash[:alert] = "#{artist.name} is already in your favorites."
-    elsif !current_user.artists.find_by(name: artist.name)
-      current_user.artists << artist
-      flash[:success] =  "#{artist.name} has been added to your favorites."
-    else
+    if !artist
       new_artist = Artist.create(name: params["artist_name"],url: params["artist_url"],followers: params["artist_followers"]) unless artist
       current_user.artists << new_artist
       flash[:success] =  "#{new_artist.name} has been added to your favorites."
+    elsif artist && current_user.artists.find_by(name: params["artist_name"])
+      flash[:alert] = "#{artist.name} is already in your favorites."
+    elsif artist && !current_user.artists.find_by(name: params["artist_name"])
+      current_user.artists << artist
+      flash[:success] =  "#{artist.name} has been added to your favorites."
     end
       redirect_to user_path(current_user.id)
   end
