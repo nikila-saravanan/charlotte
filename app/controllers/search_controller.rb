9b28cb81cc587e7781466de13459b28ab0599f4b
nikila-saravanan <nikila.saravanan@gmail.com>
2015-09-22 14:43:56 -0400

    add error handling when artist not found

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,6 +7,20 @@ class SearchController < ApplicationController
     # replace non-ASCII characters with ASCII and non-word characters with spaces for API call
     sanitized_search = I18n.transliterate(params[:q]).gsub(/(\W)/, " ")
     @artist = Search.for(sanitized_search).first
-    @related_artists = @artist.related_artists.sort_by {|artist| artist.followers["total"]}.reverse
+    if !@artist
+      begin
+        raise SearchError
+      rescue
+        flash.now[:error] = "Sorry, we couldn't find that artist. Please try again."
+      end
+    else
+      @related_artists = @artist.related_artists.sort_by {|artist| artist.followers["total"]}.reverse
+    end
   end
+
+
+end
+
+class SearchError < StandardError
+
 end
