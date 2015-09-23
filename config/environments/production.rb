3ffc0534b96e72ad017a24e24f10123d5343336b
rolandobrown <rolando.brown@gmail.com>
2015-09-22 20:33:04 -0400

    add custom font from asset pipeline

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,6 +32,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
